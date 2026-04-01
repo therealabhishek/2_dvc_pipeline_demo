9c1577ccf49c9cf5d6804a3230a1d38426970619
Abhishek Kulkarni <kul.abhishake@gmail.com>
2026-04-01 11:09:11 +0530

    params.yaml added to enable parameter imports

--- src/model_training.py
+++ src/model_training.py
@@ -119,8 +119,8 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_params('params.yaml')['model_building']
-        params = {'n_estimators': 22, 'random_state': 2}
+        params = load_params('params.yaml')['model_training']
+        #params = {'n_estimators': 22, 'random_state': 2}
         
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
@@ -132,7 +132,7 @@ def main():
         save_model(clf, model_save_path)
 
     except Exception as e:
-        logger.error('Failed to complete the model building process: %s', e)
+        logger.error('Failed to complete the model training process: %s', e)
         print(f"Error: {e}")
 
 if __name__ == '__main__':
